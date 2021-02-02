ab7f6ac63e3f378429a5ef307ae8d2ec2c340fec
Maxime Vanden Haezevelde <mvandenhaezevelde@gmail.com>
2021-02-02 14:51:58 +0100

    1-16 to be tested

--- SICP/1-16.scm
+++ SICP/1-16.scm
@@ -1,3 +1,17 @@
 #lang racket
-
-(+ 1 1)
+ (define (square x) (* x x))
+ 
+ (define (fast-expt b n) 
+   (define (cube x) (* x x x)) 
+   (define (fast-expt-iter b a counter) 
+     (cond ((= counter 0) a) 
+           ((= counter 1) (* a b)) 
+           ((even? counter) (fast-expt-iter  
+                              (square b) 
+                              (* (square b) a) 
+                              (- (/ counter 2) 1))) 
+           (else (fast-expt-iter  
+                   (square b)  
+                   (* (cube b) a) 
+                   (- (/ (- counter 1) 2) 1))))) 
+   (fast-expt-iter b 1 n)) 
